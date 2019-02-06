2a751ea429932160d15e7add5a7bde2b433d797a
Hayley Stevens <hayleycstevens@gmail.com>
2019-02-06 15:27:37 -0800

    Update embedded-video.js

--- embedded-video.js
+++ embedded-video.js
@@ -7,24 +7,27 @@
     // Tell Tableau we'd like to initialize our extension
    });
 
-	// function called by viz on marks being selected in the workbook
-function onMarksSelection(marksEvent) {
-  return marksEvent.getMarksAsync().then(reportSelectedMarks);
-}
-
-function reportSelectedMarks(marks) {
-    for (var markIndex = 0; markIndex < marks.length; markIndex++) {
-        var pairs = marks[markIndex].getPairs();
-        for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
-            var pair = pairs[pairIndex];
-            pair.formattedValue;
-            }
-        }
-    }
-}
-
-	function listenToMarksSelection() {
+        function listenToMarksSelection() {
             viz.addEventListener(tableau.TableauEventName.MARKS_SELECTION, onMarksSelection);
+        }
+        function onMarksSelection(marksEvent) {
+            return marksEvent.getMarksAsync().then(reportSelectedMarks);
+        }
+        function reportSelectedMarks(marks) {
+            var html = ""; 
+            
+            for (var markIndex = 0; markIndex < marks.length; markIndex++) {
+                var pairs = marks[markIndex].getPairs();
+                html += "<b>Mark " + markIndex + ":</b><ul>";
+                for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
+                    var pair = pairs[pairIndex];
+                    html += "<li><b>Field Name:</b> " + pair.fieldName;
+                    html += "<br/><b>Value:</b> " + pair.formattedValue + "</li>";
+                }
+                html += "</ul>";
+            }
+            var infoDiv = document.getElementById('markDetails');
+            infoDiv.innerHTML = html;
         }
 
 
