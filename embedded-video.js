0a3e645e0a605e2673f196837f39f27b63cbe21d
Hayley Stevens <hayleycstevens@gmail.com>
2019-02-06 15:00:54 -0800

    Update embedded-video.js

--- embedded-video.js
+++ embedded-video.js
@@ -5,23 +5,14 @@
   // Use the jQuery document ready signal to know when everything has been initialized
   $(document).ready(function () {
     // Tell Tableau we'd like to initialize our extension
-    const worksheetName = URL_ID;
    });
 
-            function getUnderlyingData(){
-                sheet = viz.getWorkbook().getActiveSheet().getWorksheets().get(worksheetName);
-		 // If the active sheet is not a dashboard, then you can just enter:
-		 // viz.getWorkbook().getActiveSheet();
-                options = {
-                    maxRows: 10, // Max rows to return. Use 0 to return all rows
-                    ignoreAliases: false,
-                    ignoreSelection: true,
-                    includeAllColumns: false
-                };
-    // Add an event listener for the selection changed event on this sheet.
-    unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, function (selectionEvent) {
-    });
-  }
+	function listenToMarksSelection() {
+            viz.addEventListener(tableau.TableauEventName.MARKS_SELECTION, onMarksSelection);
+        }
 
-  
+        function onMarksSelection(marksEvent) {
+            return marksEvent.getMarksAsync().then(reportSelectedMarks);
+        }
+
 )();
