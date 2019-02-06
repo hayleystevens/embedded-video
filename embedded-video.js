a94d67155040c47cb14b64bde2c511f6459bfd54
Hayley Stevens <hayleycstevens@gmail.com>
2019-02-06 14:18:11 -0800

    Update embedded-video.js

--- embedded-video.js
+++ embedded-video.js
@@ -8,11 +8,18 @@
     const worksheetName = URL_ID;
    });
 
+   tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name ===worksheetName).getUnderlyingDataAsync().then(dataTable => {
+    let field = dataTable.columns.find(column => column.fieldName === "post_or_video_id");
+    let list = [];
+    for (let row of dataTable.data) {
+      list.push(row[field.index].value);
+    }
+    let values = list.filter((el, i, arr) => arr.indexOf(el) === i);
+    console.log(values)
+  });
 
     // Add an event listener for the selection changed event on this sheet.
     unregisterEventHandlerFunction = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, function (selectionEvent) {
-      // When the selection changes, reload the data
-      loadSelectedMarks(worksheetName);
     });
   }
 
