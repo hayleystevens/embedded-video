1084ab3b6c1a89a0efe51396e319b9c998e2945c
Hayley Stevens <hayleycstevens@gmail.com>
2019-02-06 17:25:21 -0800

    Update embedded-video.js

--- embedded-video.js
+++ embedded-video.js
@@ -7,28 +7,60 @@
     // Tell Tableau we'd like to initialize our extension
    });
 
-        function listenToMarksSelection() {
-            viz.addEventListener(tableau.TableauEventName.MARKS_SELECTION, onMarksSelection);
-        }
-        function onMarksSelection(marksEvent) {
-            return marksEvent.getMarksAsync().then(reportSelectedMarks);
-        }
-        function reportSelectedMarks(marks) {
-            var html = ""; 
-            
-            for (var markIndex = 0; markIndex < marks.length; markIndex++) {
-                var pairs = marks[markIndex].getPairs();
-                html += "<b>Mark " + markIndex + ":</b><ul>";
-                for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
-                    var pair = pairs[pairIndex];
-                    html += "<li><b>Field Name:</b> " + pair.fieldName;
-                    html += "<br/><b>Value:</b> " + pair.formattedValue + "</li>";
-                }
-                html += "</ul>";
-            }
-            var infoDiv = document.getElementById('markDetails');
-            infoDiv.innerHTML = html;
-        }
+  const worksheet ='Highlight'
+  
+       function loadSelectedMarks (worksheet) {
+    // Call to get the selected marks for our sheet
+    worksheet.getSelectedMarksAsync().then(function (marks) {
+      // Get the first DataTable for our selected marks (usually there is just one)
+      const worksheetData = marks.data[0];
+
+      // Map our data into the format which the data table component expects it
+      const data = worksheetData.data.map(function (row, index) {
+        const rowData = row.map(function (cell) {
+          return cell.formattedValue;
+        });
+
+        return rowData;
+      });
+
+      const columns = worksheetData.columns.map(function (column) {
+        return { title: column.fieldName };
+      });
+
+      // Populate the data table with the rows and columns we just pulled out
+      populateDataTable(data, columns);
+    });
+  }
+
+  function populateDataTable (data, columns) {
+    // Do some UI setup here to change the visible section and reinitialize the table
+    $('#data_table_wrapper').empty();
+
+    if (data.length > 0) {
+      $('#no_data_message').css('display', 'none');
+      $('#data_table_wrapper').append(`<table id='data_table' class='table table-striped table-bordered'></table>`);
+
+      // Do some math to compute the height we want the data table to be
+      var top = $('#data_table_wrapper')[0].getBoundingClientRect().top;
+      var height = $(document).height() - top - 130;
+
+      // Initialize our data table with what we just gathered
+      $('#data_table').DataTable({
+        data: data,
+        columns: columns,
+        autoWidth: false,
+        deferRender: true,
+        scroller: true,
+        scrollY: height,
+        scrollX: true,
+        dom: "<'row'<'col-sm-6'i><'col-sm-6'f>><'row'<'col-sm-12'tr>>" // Do some custom styling
+      });
+    } else {
+      // If we didn't get any rows back, there must be no marks selected
+      $('#no_data_message').css('display', 'inline');
+    }
+  }
 
 
 )();
