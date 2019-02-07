7485ab631c8135114dce65b57322fcddcf72a4b8
Hayley Stevens <hayleycstevens@gmail.com>
2019-02-06 18:14:53 -0800

    Update embedded-video.js

--- embedded-video.js
+++ embedded-video.js
@@ -5,69 +5,9 @@
   // Use the jQuery document ready signal to know when everything has been initialized
   $(document).ready(function () {
     // Tell Tableau we'd like to initialize our extension
-    tableau.extensions.initializeAsync().then(function () {
-      // Fetch the saved sheet name from settings. This will be undefined if there isn't one configured yet
-      const savedSheetName = tableau.extensions.settings.get('sheet');
-      if (savedSheetName) {
-        // We have a saved sheet name, show its selected marks
-        loadSelectedMarks(savedSheetName);
-      } else {
-        // If there isn't a sheet saved in settings, show the dialog
-        showChooseSheetDialog();
-      }
-
-      initializeButtons();
-    });
+    
   });
-
-  /**
-   * Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
-   */
-  function showChooseSheetDialog () {
-    // Clear out the existing list of sheets
-    $('#choose_sheet_buttons').empty();
-
-    // Set the dashboard's name in the title
-    const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
-    $('#choose_sheet_title').text(dashboardName);
-
-    // The first step in choosing a sheet will be asking Tableau what sheets are available
-    const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
-
-    // Next, we loop through all of these worksheets and add buttons for each one
-    worksheets.forEach(function (worksheet) {
-      // Declare our new button which contains the sheet name
-      const button = createButton(worksheet.name);
-
-      // Create an event handler for when this button is clicked
-      button.click(function () {
-        // Get the worksheet name and save it to settings.
-        filteredColumns = [];
-        const worksheetName = worksheet.name;
-        tableau.extensions.settings.set('sheet', worksheetName);
-        tableau.extensions.settings.saveAsync().then(function () {
-          // Once the save has completed, close the dialog and show the data table for this worksheet
-          $('#choose_sheet_dialog').modal('toggle');
-          loadSelectedMarks(worksheetName);
-        });
-      });
-
-      // Add our button to the list of worksheets to choose from
-      $('#choose_sheet_buttons').append(button);
-    });
-
-    // Show the dialog
-    $('#choose_sheet_dialog').modal('toggle');
-  }
-
-  function createButton (buttonTitle) {
-    const button =
-    $(`<button type='button' class='btn btn-default btn-block'>
-      ${buttonTitle}
-    </button>`);
-
-    return button;
-  }
+const worksheetName = 'URL_ID'
 
   // This variable will save off the function we can call to unregister listening to marks-selected events
   let unregisterEventHandlerFunction;
@@ -80,9 +20,6 @@
 
     // Get the worksheet object we want to get the selected marks for
     const worksheet = getSelectedSheet(worksheetName);
-
-    // Set our title to an appropriate value
-    $('#selected_marks_title').text(worksheet.name);
 
     // Call to get the selected marks for our sheet
     worksheet.getSelectedMarksAsync().then(function (marks) {
